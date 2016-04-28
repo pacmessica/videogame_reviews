882bfa197b803b3c567b128d9b00d2658c43feac
Jessica Paczuski <jz.paczuski@gmail.com>
2016-04-28 12:28:53 +0200

    add game seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,18 @@
 print "Seeding standard categories..."
 
-Category.create([
-  { name:"PC Games"},
-  { name:"Xbox Games" },
-  { name:"Playstation Games"},
-  { name: "Wii Games" }
-  ])
+pc_games = Category.create( name:"PC Games")
+xbox_games = Category.create( name:"Xbox Games")
+playstation_games = Category.create( name:"Playstaion Games")
+board_games = Category.create( name:"Board Games")
 
-  puts "seeded #{Category.count} categories!"
+puts "seeded #{Category.count} categories!"
+
+Game.create(title: "Pong", category: pc_games )
+Game.create(title: "Commandos", category: pc_games)
+Game.create(title: "Settlers", category: pc_games)
+
+Game.create(title: "Splinter Cells", category: xbox_games)
+
+Game.create(title: "Mortal Combat", category: playstation_games)
+
+Game.create(title: "Risk", category: board_games)
