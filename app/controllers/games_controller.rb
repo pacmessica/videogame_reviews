ac36d71b0e747aa873800403c87cafb5a9a45ba6
Jessica Paczuski <jz.paczuski@gmail.com>
2016-04-28 16:18:28 +0200

    fix controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,6 +3,11 @@ class GamesController < ApplicationController
 
   def index
     @games = @category.games
+
+    respond_to do |f|
+      f.html
+      f.json { render json: { category: @category, games: @games } }
+    end
   end
 
   def show
