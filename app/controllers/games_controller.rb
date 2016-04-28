64ddece51ab6d86ff8922659f51108477b837e7a
Jessica Paczuski <jz.paczuski@gmail.com>
2016-04-28 14:41:56 +0200

    fix game controllers

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,11 +1,29 @@
 class GamesController < ApplicationController
+  before_filter :set_category
+
   def index
-    @category = Category.find(params[:category_id])
     @games = @category.games
   end
 
   def show
-    @category = Category.find(params[:category_id])
     @game = @category.games.find(params[:id])
   end
+
+
+  def create
+    @game = Game.new(game_params)
+    @game.category = @category
+
+    if @game.save
+      redirect_to category_game_path(@category, @game), notice: "Game created successfully"
+    else
+      render :new
+    end
+  end
+
+  protected
+  def set_category
+    @category = Category.find(params[:category_id])
+  end
+
 end
