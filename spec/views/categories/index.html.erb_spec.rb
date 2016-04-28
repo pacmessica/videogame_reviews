e206f6289b2295a24c6322c3ea4b63f58d9b4692
pacmessica <jz.paczuski@gmail.com>
2016-04-28 12:08:09 +0200

    [WIP]Categories (#1)
    * Add Facotry Girl Rails gem for fixtures
    
    * add category model with validation
    
    * Category index action and routing
    
    * Category index view

---
+++ spec/views/categories/index.html.erb_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe "categories/index.html.erb", type: :view do
+  let(:categories) { create_list(:category, 3) }
+  before do
+    assign(:categories, categories)
+    render
+  end
+
+  it "shows a title" do
+    assert_select 'h1', text: "Video Games", count: 1
+  end
+
+  it "renders all the categories" do
+    categories.each do |category|
+      assert_select 'h3', :text => category.name, count: 1
+    end
+  end
+end
